4ec63de00269ea3d491dbdc7f61d87cee7de3612
sveltemaster <sveltemaster@gmail.com>
2019-10-05 11:59:25 -0700

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>HI NETLIFY!</h1>
+
+<figure>
+	<img alt='cartoon fly' src='https://www.clipartwiki.com/clipimg/detail/238-2383171_flying-clipart-cute-cute-fly-clipart.png'>
+	<figcaption>A fly, more specifically, a netflify ;D</figcaption>
+</figure>
+
