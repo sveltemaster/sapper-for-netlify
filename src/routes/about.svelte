675a79f4c602451b8a381bc7e159c7c7e262754d
sveltemaster <sveltemaster@gmail.com>
2020-09-07 10:44:17 -0700

    workplz

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,4 +1,5 @@
-<form name="form" netlify>
-
-    <input type="email">
+<form action="/success" name="form" method="POST" netlify netlify-honeypot="bot-field">
+    <input style="opacity: 0" name="bot-field" />
+    <input type="text" required>
+    <button type="submit">Submit</button>
 </form>
